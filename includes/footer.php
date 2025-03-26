2919c373609504532b568af5424b39a7f0b0e55d
Mike Adrian <mikeadrian123456@gmail.com>
2025-03-26 10:59:48 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -18,22 +18,22 @@
                 <h3>Quick Links</h3>
                 <ul class="footer-links">
                     <li><a href="#">Home</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#appointment">Appointment</a></li>
-                    <li><a href="#about">About Us</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="../services.php">Services</a></li>
+                    <li><a href="../appointment.php">Appointment</a></li>
+                    <li><a href="../about.php">About Us</a></li>
+                    <li><a href="../contact.php">Contact</a></li>
                 </ul>
             </div>
 
             <div class="footer-column">
                 <h3>Services</h3>
                 <ul class="footer-links">
-                    <li><a href="#gate-automation">Gate Automation</a></li>
-                    <li><a href="#gate-fabrication">Gate Fabrication</a></li>
-                    <li><a href="#sectional-garage">Sectional Garage Door</a></li>
-                    <li><a href="#roll-up">Roll Up Door</a></li>
-                    <li><a href="#electric-fence">Smart Electric Fence</a></li>
-                    <li><a href="#smart-curtain">Smart Curtain</a></li>
+                    <li><a href="../services/gate automation.php">Gate Automation</a></li>
+                    <li><a href="../services/gate_fabrication.php">Gate Fabrication</a></li>
+                    <li><a href="../services/garage_door.php">Sectional Garage Door</a></li>
+                    <li><a href="../services/roll_up_door.php">Roll Up Door</a></li>
+                    <li><a href="../services/electric_fence.php">Smart Electric Fence</a></li>
+                    <li><a href="../services/smart_curtain.php">Smart Curtain</a></li>
                 </ul>
             </div>
 
@@ -42,15 +42,15 @@
                 <ul class="footer-contact-info">
                     <li>
                         <i class="fas fa-map-marker-alt"></i>
-                        <span>123 Industrial Ave, Business District, City</span>
+                        <span>Pugpo Sta. Cruz Angat Bulacan </span>
                     </li>
                     <li>
                         <i class="fas fa-phone"></i>
-                        <span>+1 (123) 456-7890</span>
+                        <span>+1 (123) 456-7880</span>
                     </li>
                     <li>
                         <i class="fas fa-envelope"></i>
-                        <span>info@IStech.com</span>
+                        <span>intellitectsystem@IStech.com</span>
                     </li>
                 </ul>
             </div>
